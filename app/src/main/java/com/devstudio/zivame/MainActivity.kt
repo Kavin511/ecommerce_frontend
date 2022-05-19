bcc4795460f9f9bee26d477f70cd41d904925ed5
kavin511 <kavinalmighty@gmail.com>
2022-05-19 19:50:57 +0530

    Kavin | Added cart icon and item cound badge

--- app/src/main/java/com/devstudio/zivame/MainActivity.kt
+++ app/src/main/java/com/devstudio/zivame/MainActivity.kt
@@ -2,20 +2,21 @@ package com.devstudio.zivame
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.MenuItemCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.devstudio.zivame.cart.ShoppingCart
-import com.devstudio.zivame.cart.ShoppingCartItem
 import com.devstudio.zivame.models.Product
 import com.devstudio.zivame.repository.ProductRepository
 import com.devstudio.zivame.service.ProductService
@@ -23,17 +24,61 @@ import com.devstudio.zivame.viewmodels.ProductViewModel
 import com.devstudio.zivame.viewmodels.ProductViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var badgeText: TextView
+    private lateinit var badgeLayout: FrameLayout
     private lateinit var productList: RecyclerView
     private lateinit var adapter: ProductListAdapter
+    private var cartMenu: Menu? = null
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.cart_menu, menu)
+        cartMenu = menu
+        initialiseCartMenu()
+        updateBadgeCount()
+        return super.onCreateOptionsMenu(cartMenu)
+    }
+
+    private fun initialiseCartMenu() {
+        val item: MenuItem = cartMenu!!.findItem(R.id.badge)
+        MenuItemCompat.setActionView(item, R.layout.actionbar_badge_layout)
+        badgeLayout = cartMenu?.findItem(R.id.badge)?.actionView as FrameLayout
+        badgeText = badgeLayout.findViewById<View>(R.id.actionbar_notifcation_textview) as TextView
+    }
+
+    private fun updateBadgeCount() {
+        val size = ShoppingCart.getInstance()?.size
+        badgeText.text = size.toString()
+        if (size == 0) {
+            badgeText.visibility = GONE
+        }
+        else {
+            badgeText.visibility = VISIBLE
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialise()
+        val viewModel = initialiseProductViewModel()
+        observeProductList(viewModel)
+        initialiseProductList(viewModel)
+    }
+
+    private fun initialise() {
+        productList = findViewById(R.id.products_list)
+    }
+
+    private fun initialiseProductViewModel(): ProductViewModel {
         val productService = ProductService.getInstance()
         val viewModel = ViewModelProvider(
             this,
             ProductViewModelFactory(ProductRepository(productService = productService))
         ).get(ProductViewModel::class.java)
-        productList = findViewById(R.id.products_list)
+        return viewModel
+    }
+
+    private fun observeProductList(viewModel: ProductViewModel) {
+        viewModel.fetchProducts()
         viewModel.productList.observe(this) {
             adapter = ProductListAdapter(
                 viewModel.productList.value ?: listOf(),
@@ -42,7 +87,9 @@ class MainActivity : AppCompatActivity() {
             )
             productList.adapter = adapter
         }
-        viewModel.fetchProducts()
+    }
+
+    private fun initialiseProductList(viewModel: ProductViewModel) {
         adapter = ProductListAdapter(
             viewModel.productList.value ?: listOf(),
             applicationContext,
@@ -54,20 +101,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun productClickListener() = object : OnItemClickListener {
         override fun onItemClick(item: Product) {
-            val cart = ShoppingCart.getInstance()
-            var position: Long = cart?.size?.toLong() ?: 0;
-            cart?.forEach { _, shoppingCartItem ->
-                if (shoppingCartItem.id == item.id) {
-                    position = shoppingCartItem.position
-                    return@forEach
-                }
-            }
-            val item: ShoppingCartItem =
-                cart?.get(position) ?: ShoppingCartItem(item.id!!, 0, position)
-            item.quantity += 1
-            cart?.put(position, item)
-            Log.d("TAG", "onItemClick: " + cart.toString())
+            addItemToShoppingCart(item)
         }
+    }
+
+    private fun addItemToShoppingCart(item: Product) {
+        val cart = ShoppingCart.getInstance()
+        cart?.addItem(item)
+        updateBadgeCount()
     }
 
     internal class ProductListAdapter(
--- app/src/main/java/com/devstudio/zivame/cart/ShoppingCart.kt
+++ app/src/main/java/com/devstudio/zivame/cart/ShoppingCart.kt
@@ -1,11 +1,30 @@
 package com.devstudio.zivame.cart
 
+import com.devstudio.zivame.models.Product
 import java.io.Serializable
 import java.util.*
 import kotlin.collections.HashMap
 import kotlin.collections.LinkedHashMap
 
 class ShoppingCart : LinkedHashMap<Long, ShoppingCartItem>(), Serializable {
+    fun addItem(item: Product) {
+        val position = findItemPositionInCart(item)
+        val shoppingCartItem: ShoppingCartItem = get(position) ?: ShoppingCartItem(item.id!!, 0, position)
+        shoppingCartItem.quantity += 1
+        put(position, shoppingCartItem)
+    }
+
+    private fun findItemPositionInCart(item: Product): Long {
+        var position: Long = size.toLong()
+        forEach { _, shoppingCartItem ->
+            if (shoppingCartItem.id == item.id) {
+                position = shoppingCartItem.position
+                return@forEach
+            }
+        }
+        return position
+    }
+
     companion object {
         private var instance: ShoppingCart? = null
         fun getInstance(): ShoppingCart? {
